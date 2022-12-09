3006719e58e2efa3d8b5b33faeff1d07cba81b14
Deep <deepp3021@gmail.com>
2022-12-09 23:45:22 +0530

    Working on the form for signin page, setting up useState for input controll

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import FooterContainer from "../containers/footer";
 import HeaderContainer  from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin(){
+    const [emailAddress, setEmailAddress] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState('');
+
+    // check form input elements are valid
+    // email and password 
+
     return(
         <>
         <HeaderContainer>Hello from the signin!</HeaderContainer>
+        <Form>
+            <Form.Title>Sign IN</Form.Title>
+            {error && <Form.Error>{error}</Form.Error>}
+        </Form>
         <FooterContainer/>
         </>
     )
