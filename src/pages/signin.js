c8bb6ef216e079dd435148f30661a2e7fcb92625
Deep <deepp3021@gmail.com>
2022-12-11 00:16:50 +0530

    working on error handling and input functions in form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import FooterContainer from "../containers/footer";
 import HeaderContainer  from "../containers/header";
-import { Form } from "../components";
+import { Form } from "../components/index";
 
 export default function Signin(){
     const [emailAddress, setEmailAddress] = useState();
@@ -9,15 +9,36 @@ export default function Signin(){
     const [error, setError] = useState('');
 
     // check form input elements are valid
+    const isInvalid = password === '' || emailAddress === '';
+    const handleSignIn = (event) => {
+        event.preventDefault();
+        // Firebase work in here
+    }
     // email and password 
+    
 
     return(
         <>
-        <HeaderContainer>Hello from the signin!</HeaderContainer>
+        <HeaderContainer>Hello from the signin!
         <Form>
             <Form.Title>Sign IN</Form.Title>
             {error && <Form.Error>{error}</Form.Error>}
+            <Form.Base onSubmit = {handleSignIn} method = "POST">
+                <Form.Input 
+                    placeholder= "Email Address"
+                    value = {emailAddress}
+                    onChange = {({target}) => setEmailAddress(target.value)} />
+                <Form.Input 
+                    placeholder= "Password"
+                    type = "password"
+                    autoComplete = "off"
+                    value = {password}
+                    onChange = {({target}) => setPassword(target.value)} />
+                <Form.Submit disabled = {isInvalid} type= "submit" >Sign IN</Form.Submit>
+            </Form.Base>
         </Form>
+        </HeaderContainer>
+        
         <FooterContainer/>
         </>
     )
